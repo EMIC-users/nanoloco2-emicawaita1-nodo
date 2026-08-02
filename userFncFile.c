c9fc04535d0a9ef0884a140899b16f3003f7f2f3
EMIC Compiler <emic-compiler@emic.io>
2026-08-02 20:12:38 -0300

    Code generation: 2026-08-02 23:12:38 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: nodo
    Project: emicawaita1
    User: nanoloco2

--- userFncFile.c
+++ userFncFile.c
@@ -80,7 +80,6 @@ static void etOut1_awaitFsm(void)
         return;
     case 4:
         LEDs_led_state(0);
-        // Prueba mudanza paleta
     }
     /* fin del bloque: la tarea vuelve a idle */
     etOut1_awaitCtx.state = 0;
